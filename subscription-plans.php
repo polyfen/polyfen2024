a51a0e8a6345f827420902cce498fa55bd6d0c81
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-25 16:57:33 +0200

    add: subscriptions page (initial commit)

---
+++ subscription-plans.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Subscription Plans for Start-ups | Polyfen</title>
+
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+    <!-- PAGE-SPECIFIC META start -->
+    <meta name="robots" content="index, follow">
+    <meta property="og:url" content="https://polyfen.com/subscription-plans">
+    <meta property="og:title" content="Subscription Plans for Start-ups">
+    <meta name="twitter:title" content="Subscription Plans for Start-ups">
+    <meta name="description" property="og:description"
+        content="">
+    <meta name="twitter:description"
+        content="">
+    <meta name="keywords" content="">
+    <meta property="og:image" content="https://polyfen.com/imgs/meta/#.png">
+    <meta name="twitter:image" content="https://polyfen.com/imgs/meta/#.png">
+    <!-- PAGE-SPECIFIC META end -->
+
+    <!-- PAGE-SPECIFIC CSS start -->
+    <!-- PAGE-SPECIFIC CSS end -->
+
+    <!-- PAGE-SPECIFIC SCRIPTS start -->
+    <!-- PAGE-SPECIFIC SCRIPTS end -->
+
+</head>
+
+<body class="discovery-call landing-page dark-mode">
+    <!-- ya hay un estilo del founder-social  que llama a este ID -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- INCLUDE HELLO-BAR.PHP start -->
+    <?php include 'includes/hello-bar.php';?>
+    <!-- INCLUDE HELLO-BAR.PHP end -->
+
+    <!-- INCLUDE HEADER.PHP start -->
+    <?php include 'includes/header.php';?>
+    <!-- INCLUDE HEADER.PHP end -->
+
+    <!-- Discovery Call - Landing page start -->
+    <main class="">
+        
+        <section class="container-1080">
+            <h1 class="caption align-center">Subscriptions</h1>
+            <h2 class="heading-1 align-center">Affordable Plans for<br>Every Start-up</h2>
+            <section class="row">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Starter</h3>
+                        <h4>$4917</h4>
+                        <small>/Month</small>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>1 task at a time</li>
+                            <li>3-5 days turnaround</li>
+                            <li>Unlimited tasks by Devs and Designers</li>
+                            <li>Unlimited revisions</li>
+                            <li>Unlimited projects</li>
+                            <li>Pause or cancel any time</li>
+                        </ul>
+                        <a class="button secondary-button small-button">Start Today ➞</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Pro</h3>
+                        <h4>$8945</h4>
+                        <small>/Month</small>
+                        <small>Popular</small>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>2 task at a time</li>
+                            <li>2-4 days turnaround</li>
+                            <li>Unlimited tasks by Devs, Designers, and Ops</li>
+                            <li>Priority support</li>
+                            <li>Unlimited revisions</li>
+                            <li>Unlimited projects</li>
+                            <li>Pause or cancel any time</li>
+                        </ul>
+                        <a class="button secondary-button small-button">Start Today ➞</a>
+                    </div>
+                </div>
+                <div class="custom-plans">
+                    <h3 class="caption">Want more?</h3>
+                    <h3 class="heading-4">Talk to Sales for a Custom Plan</h3>
+                    <a class="button secondary-button small-button">Book a Call ➞</a>
+                    <img src="" alt="">
+                </div>
+            </section>
+        </section>
+
+        <hr>
+
+        <section class="container-1080 row">
+            <div class="col">
+                <h2></h2>
+                <p></p>
+            </div>
+            <div class="col">
+                <h2></h2>
+                <p></p>
+            </div>
+            <div class="col">
+                <h2></h2>
+                <p></p>
+            </div>
+        </section>
+
+        <?php include 'includes/call-to-action.php';?>
+
+    </main>
+    <!-- FORCE MAX-WIDTH 100% end -->
+
+    <!-- INCLUDE FOOTER.PHP start -->
+    <?php include 'includes/footer.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
+
+</body>
+
+</html>
