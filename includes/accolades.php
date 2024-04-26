60592ec8902b04fc1fc555134f1106d51aebdb11
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-26 18:19:37 +0200

    improve: subscriptions page

---
+++ includes/accolades.php
@@ -0,0 +1,9 @@
+<section class="clients-featured container-900 hidden">
+    <h2 class="caption align-center">Clients featured in</h2>
+    <div class="clients-container">
+        <img src="imgs/fortune-500-logo.svg" alt="Fortune 500" height="60" class="hidden">
+        <img src="imgs/y-combinator-logo.svg" alt="Y Combinator" height="50" class="hidden">
+        <img src="imgs/forbes-logo.svg" alt="Forbes" height="40" class="hidden">
+        <img src="imgs/tech-cruch-disrupt-logo.svg" alt="TechCrunch Disrupt" height="45" class="hidden">
+    </div>
+</section>
--- subscriptions.php
+++ subscriptions.php
@@ -11,7 +11,7 @@
 
     <!-- PAGE-SPECIFIC META start -->
     <meta name="robots" content="index, follow">
-    <meta property="og:url" content="https://polyfen.com/subscription-plans">
+    <meta property="og:url" content="https://polyfen.com/subscriptions">
     <meta property="og:title" content="Subscription Plans for Start-ups">
     <meta name="twitter:title" content="Subscription Plans for Start-ups">
     <meta name="description" property="og:description"
@@ -54,17 +54,17 @@
             <h1 class="caption align-center">Subscriptions</h1>
             <h2 class="heading-1 align-center">Affordable Plans for<br>Every Start-up</h2>
             <section class="row">
-                <div class="card">
+                <div class="subscription-plan-card">
                     <div class="card-header">
-                        <h3>Starter</h3>
-                        <h4>$4917</h4>
-                        <small>/Month</small>
+                        <h3 class="heading-2">Starter</h3>
+                        <h4 class="heading-3">$4917</h4>
+                        <small class="caption">/Month</small>
                     </div>
                     <div class="card-body">
-                        <ul>
-                            <li>1 task at a time</li>
-                            <li>3-5 days turnaround</li>
-                            <li>Unlimited tasks by Devs and Designers</li>
+                        <ul class="hanging-list">
+                            <li class="bold">1 task at a time</li>
+                            <li class="bold">3-5 days turnaround</li>
+                            <li class="bold">Unlimited tasks by Devs and Designers</li>
                             <li>Unlimited revisions</li>
                             <li>Unlimited projects</li>
                             <li>Pause or cancel any time</li>
@@ -72,19 +72,19 @@
                         <a class="button secondary-button small-button">Start Today ➞</a>
                     </div>
                 </div>
-                <div class="card">
+                <div class="subscription-plan-card featured-card">
                     <div class="card-header">
-                        <h3>Pro</h3>
-                        <h4>$8945</h4>
-                        <small>/Month</small>
-                        <small>Popular</small>
+                        <h3 class="heading-2">Pro</h3>
+                        <h4 class="heading-3">$8945</h4>
+                        <small class="caption">/Month</small>
+                        <small class="card-label mini">Popular</small>
                     </div>
                     <div class="card-body">
-                        <ul>
-                            <li>2 task at a time</li>
-                            <li>2-4 days turnaround</li>
-                            <li>Unlimited tasks by Devs, Designers, and Ops</li>
-                            <li>Priority support</li>
+                        <ul class="hanging-list">
+                            <li class="bold">2 task at a time</li>
+                            <li class="bold">2-4 days turnaround</li>
+                            <li class="bold">Unlimited tasks by Devs, Designers, and Ops</li>
+                            <li class="bold">Priority support</li>
                             <li>Unlimited revisions</li>
                             <li>Unlimited projects</li>
                             <li>Pause or cancel any time</li>
@@ -93,10 +93,10 @@
                     </div>
                 </div>
                 <div class="custom-plans">
-                    <h3 class="caption">Want more?</h3>
-                    <h3 class="heading-4">Talk to Sales for a Custom Plan</h3>
-                    <a class="button secondary-button small-button">Book a Call ➞</a>
-                    <img src="" alt="">
+                    <h3 class="caption" style="margin-bottom: 8px;">Want more?</h3>
+                    <h4 class="heading-2">Talk to Sales for a Custom Plan</h4>
+                    <a class="button secondary-button small-button margin-right-auto">Book a Call ➞</a>
+                    <img src="imgs/coin-illustration.png" alt="" class="coin-illustration">
                 </div>
             </section>
         </section>
@@ -105,18 +105,32 @@
 
         <section class="container-1080 row">
             <div class="col">
-                <h2></h2>
-                <p></p>
+                <h2>10x Professionals</h2>
+                <p>We hire world-class engineers, design and UX professionals in your timezone.</p>
             </div>
             <div class="col">
-                <h2></h2>
-                <p></p>
+                <h2>Fast-track Onboarding</h2>
+                <p>No contracts, no paperwork hassle. Start today.</p>
             </div>
             <div class="col">
-                <h2></h2>
-                <p></p>
+                <h2>Devs, Ops, & Designers</h2>
+                <p>A single subscription for tasks of all kinds.</p>
             </div>
         </section>
+
+        <hr>
+
+        <section class="container-1080">
+            <h2 class="align-center">Solutions for<br>Every Stack</h2>
+        </section>
+
+        <hr>
+
+        <section class="container-1080">
+            <h2 class="align-center">Frequently Asked Questions</h2>
+        </section>
+
+        <?php include 'includes/accolades.php';?>
 
         <?php include 'includes/call-to-action.php';?>
 
