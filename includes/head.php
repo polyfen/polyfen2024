42cbecd8b9ca02efae99fc8ef473cd2ba0e4d82e
Sebastian Bocchio Oguic <sbocchio@gmail.com>
2024-04-30 13:02:09 +0200

    social-icons-svg
    # Changing social icon from tipography for svg formats
    *Sebo - 21/04/2024*
    
    ## Nuevos archivos
    ### Nueva carpeta de íconos
    
    Se descargaron svg y se adecuaron para reemplazar en footer.php los íconos de social-media.
    
    ### Descarga de íconos
    
    #### Icono Github
    Descargado de iconfinder.com -> Collección _ionicons_, con licencia MIT
    [https://www.iconfinder.com/iconsets/ionicons]
    
    #### Icono X
    Descargado de logowik.com
    [https://logowik.com/twitter-x-logo-vector-59293.html]
    
    #### Icono Linkedin
    Descargado de iconfinder.com -> Colección _Social Icons icon pack_ de _Rebin Infotech_, con licencia Creative Commons
    [https://www.iconfinder.com/iconsets/social-icons-32]
    
    #### Icono Instagram
    Descargado de logowik.com
    [https://logowik.com/instagram-black-line-logo-vector-svg-pdf-ai-eps-cdr-free-download-15083.html]
    
    ## Adecuaciones de código
    ### footer.php
    Se modificó el elemento _ul_ con los accesos a las redes sociales e íconos
    Se cargaron los nuevos íconos en blanco, se adecuaron los svg para respetar el color de font del sitio
    
    ### Nueva clase social_icon
    Para que los nuevos svg se visualicen a la par y de igual tamaño, asigné a los 4 una nueva clase _social_icon_ que agregué en custom_styles.cs_. En dicha clase se fija un tamaño cuadrado de 25px con un _object-fit:cover_ para que si el svg excede en los lados tome el fragmento central adaptandolo a 25px. Tanto en Desktop como en Mobile pareciera responder bien ese tamaño.
    
    ## Resta
    - Stagear
        - footer.php
        - custom_styles.css
        - .icons\*.svg (4 nuevos)
    
    ## Eliminación de la referencia a fontawesome
    Se elimina la referencia a Fontawesome dentro de head.php
    
    _Sebo_

--- includes/head.php
+++ includes/head.php
@@ -32,8 +32,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<script src="https://kit.fontawesome.com/d00980734d.js" crossorigin="anonymous"></script>
+
 <!-- GTM -->
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
