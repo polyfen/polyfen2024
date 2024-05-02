ab50fb9dc3897d8b041b11bbf189b7914da5c12a
xxiicode <leandrobilokapic@gmail.com>
2024-05-02 17:34:28 +0200

    improve: landing-page tech stack

--- subscriptions.php
+++ subscriptions.php
@@ -27,6 +27,11 @@
     <!-- PAGE-SPECIFIC SCRIPTS start -->
     <!-- PAGE-SPECIFIC SCRIPTS end -->
 
+    <!-- slick -->
+    <link rel="stylesheet" type="text/css" href="/libs/slick/slick/slick.css" />
+    // Add the new slick-theme.css if you want the default styling
+    <link rel="stylesheet" type="text/css" href="/libs/slick/slick/slick-theme.css" />
+
 </head>
 
 <body class="landing-page subscriptions dark-mode">
@@ -120,8 +125,145 @@
 
         <section class="container-1080">
             <h2 class="align-center">Solutions for<br>Every Stack</h2>
+            <div class="stack-container container-800">
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/git-original.svg">Git</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/jira-original.svg">Jira</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/confluence-original.svg">Confluence</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/wordpress-plain.svg">Wordpress</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/woocommerce-original.svg">WooCommerce
+                </div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/javascript-original.svg">JavaScript</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/nodejs-original.svg">Node.js</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/react-original.svg">React</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/typescript-original.svg">TypeScript</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/vuejs-original.svg">Vue.js</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/angular-original.svg">Angular</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/python-plain.svg">Python</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/php-original.svg">PHP</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/laravel-original.svg">Laravel</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/mongodb-original.svg">MongoDB</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/firebase-original.svg">Firebase</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/nextjs-plain.svg">Next.js</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/vercel-original.svg">Vercel</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/bun-original.svg">Bun</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/swift-original.svg">Swift</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/kotlin-original.svg">Kotlin</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/dart-original.svg">Dart</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/flutter-original.svg">Flutter</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/rails-original-wordmark.svg">Ruby on
+                    Rails</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/sass-original.svg">Sass</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/bootstrap-original.svg">Bootstrap</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/tailwindcss-original.svg">Tailwind</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/materialui-plain.svg">Material UI</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/figma-original.svg">Figma</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/framer.svg">Framer</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/illustrator-plain.svg">Illustrator</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/photoshop-original.svg">Photoshop</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/aftereffects-original.svg">After Effects
+                </div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/premierepro-original.svg">Premiere Pro
+                </div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/hotjar-logo.svg">Hotjar</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/aws-plain.svg">AWS</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/azure-original.svg">Azure</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/googlecloud-original.svg">Google Cloud
+                </div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/docker-original.svg">Docker</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/kubernetes-original.svg">Kubernetes</div>
+                <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/jenkins-original.svg">Jenkins</div>
+            </div>
         </section>
-
+        <!-- Tech Stack for carrousel -->
+        <div class="stack-carrousel container-800">
+            <div class="carrousel-1">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/git-original.svg">Git</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/jira-original.svg">Jira</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/confluence-original.svg">Confluence
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/wordpress-plain.svg">Wordpress</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/woocommerce-original.svg">WooCommerce
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/javascript-original.svg">JavaScript
+                    </div>
+                </div>
+            </div>
+            <div class="carrousel-2">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/nodejs-original.svg">Node.js</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/react-original.svg">React</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/typescript-original.svg">TypeScript
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/vuejs-original.svg">Vue.js</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/angular-original.svg">Angular</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/python-plain.svg">Python</div>
+                </div>
+            </div>
+            <div class="carrousel-3">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/php-original.svg">PHP</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/laravel-original.svg">Laravel</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/mongodb-original.svg">MongoDB</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/firebase-original.svg">Firebase</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/nextjs-plain.svg">Next.js</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/vercel-original.svg">Vercel</div>
+                </div>
+            </div>
+            <div class="carrousel-4">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/bun-original.svg">Bun</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/swift-original.svg">Swift</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/kotlin-original.svg">Kotlin</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/dart-original.svg">Dart</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/flutter-original.svg">Flutter</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/rails-original-wordmark.svg">Ruby on
+                        Rails</div>
+                </div>
+            </div>
+            <div class="carrousel-5">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/sass-original.svg">Sass</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/bootstrap-original.svg">Bootstrap
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/tailwindcss-original.svg">Tailwind
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/materialui-plain.svg">Material UI
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/figma-original.svg">Figma</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/framer.svg">Framer</div>
+                </div>
+            </div>
+            <div class="carrousel-6">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/illustrator-plain.svg">Illustrator
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/photoshop-original.svg">Photoshop
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/aftereffects-original.svg">After
+                        Effects
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/premierepro-original.svg">Premiere
+                        Pro
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/hotjar-logo.svg">Hotjar</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/aws-plain.svg">AWS</div>
+                </div>
+            </div>
+            <div class="carrousel-7">
+                <div class="slide-inside">
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/azure-original.svg">Azure</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/googlecloud-original.svg">Google
+                        Cloud
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/docker-original.svg">Docker</div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/kubernetes-original.svg">Kubernetes
+                    </div>
+                    <div class="stack-u"><img loading="lazy" src="/imgs/tech-stack/jenkins-original.svg">Jenkins</div>
+                </div>
+            </div>
+        </div>
+        <!-- Tech carrousel END -->
         <hr>
 
         <section class="faqs container-1080 hidden">
@@ -132,7 +274,10 @@
                         <h3 class="heading-4"><span>1. </span>What kind of tasks can I submit?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>As</p>
+                        <p>We can help you with any kind of task that you need help with, from building a new feature,
+                            to fixing a bug, to refactoring your codebase, to setting up your AWS infrastructure (only
+                            available on the Pro plan), and more. If you're not sure if we can help you with your task,
+                            just ask us.</p>
                     </div>
                 </div>
 
@@ -141,7 +286,9 @@
                         <h3 class="heading-4"><span>2. </span>Can I build an entire app or MVP with you?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>Together</p>
+                        <p>Yes, we can help you build your entire app or MVP, just submit your tasks and we'll get to
+                            work. We are going to slice your project into small tasks and work through them one or two
+                            at a time (it depends on your plan).</p>
                     </div>
                 </div>
 
@@ -150,7 +297,9 @@
                         <h3 class="heading-4"><span>3. </span>Can I pause or cancel my subscription at anytime?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>Yes, you can pause or cancel your subscription at anytime, no questions asked. If you pause
+                            or cancel your subscription, you will still have access to your account until the end of
+                            your billing cycle.</p>
                     </div>
                 </div>
 
@@ -159,7 +308,7 @@
                         <h3 class="heading-4"><span>4. </span>Can I change my subscription plan?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>Together</p>
+                        <p>Yes, you can upgrade or downgrade your subscription plan at anytime.</p>
                     </div>
                 </div>
 
@@ -168,7 +317,8 @@
                         <h3 class="heading-4"><span>5. </span>What if I need more than 2 tasks at a time?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>We offer a custom plan for startups that need more than 2 tasks at a time, please contact us
+                            for more information.</p>
                     </div>
                 </div>
 
@@ -177,7 +327,7 @@
                         <h3 class="heading-4"><span>6. </span>Can I ask you to follow my coding standards?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>Yes, we can follow your coding standards, just let us know what they are.</p>
                     </div>
                 </div>
 
@@ -186,7 +336,8 @@
                         <h3 class="heading-4"><span>7. </span>Can I prioritize my tasks?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>Yes, as soon as you pay, you'll have access to our Notion kanban board where you can submit
+                            and prioritize your tasks.</p>
                     </div>
                 </div>
 
@@ -195,7 +346,8 @@
                         <h3 class="heading-4"><span>8. </span>Can I cancel a task once it's started?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>Yes, you're in control of your tasks, projects, and priorities, you can cancel a task at
+                            anytime.</p>
                     </div>
                 </div>
 
@@ -204,7 +356,10 @@
                         <h3 class="heading-4"><span>9. </span>What technologies do you use?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>Our engineers are experienced with the following technologies: React, Next.js, Node.js,
+                            Laravel, PHP, Airflow, Java, Express.js, MongoDB, PostgreSQL, MySQL, AWS (Lambdas, Step
+                            Functions, etc), Vercel, Render, and more. If you need help with a technology that's not
+                            listed here, just ask us.</p>
                     </div>
                 </div>
 
@@ -213,7 +368,7 @@
                         <h3 class="heading-4"><span>10. </span>Can you sign an NDA?</h3>
                     </div>
                     <div class="acc-panel">
-                        <p>We</p>
+                        <p>Yes, we can sign an NDA, just send it to us.</p>
                     </div>
                 </div>
             </div>
@@ -240,6 +395,17 @@
     <?php include 'includes/footer.php';?>
     <!-- INCLUDE FOOTER.PHP end -->
 
+    <!-- slick -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="libs/slick/slick/slick.min.js"></script>
+    <script>
+    $(document).ready(function() {
+        $('.stack-carrousel').slick({
+
+        });
+    });
+    </script>
 </body>
 
 </html>
