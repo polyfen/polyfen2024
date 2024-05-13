fbf6a06c2713792e1030a9bdc4a7730c2cd849c8
xxiicode <leandrobilokapic@gmail.com>
2024-05-13 14:25:32 +0200

    improve: indentation for FAQs

--- app/js/subtle.js
+++ app/js/subtle.js
@@ -1,7 +1,7 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('visible');
         } else {
             entry.target.classList.remove('visible');
         }
