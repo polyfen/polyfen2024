941d28a162fcfed590805b1064da7f5fe0c15c5f
xxiicode <leandrobilokapic@gmail.com>
2024-05-13 14:28:37 +0200

    hotfix

--- app/js/subtle.js
+++ app/js/subtle.js
@@ -1,9 +1,9 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('visible');
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('visible');
         }
     });
 });
@@ -17,7 +17,7 @@ fadeinElements.forEach((el) => observer.observe(el));
 const observerHeader = new IntersectionObserver(entries => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('visible');
         }
     });
 });
