8823724e342c8619df43a16d0e87fa33ae3cb091
xxiicode <leandrobilokapic@gmail.com>
2024-05-15 16:50:17 +0200

    powell

--- includes/accolades.php
+++ includes/accolades.php
@@ -1,9 +1,17 @@
-<section class="clients-featured container-900 fade-in">
+<section class="clients-featured container-900 ">
     <h2 class="caption align-center">Clients featured in</h2>
-    <div class="clients-container">
-        <img src="imgs/fortune-500-logo.svg" alt="Fortune 500" height="60" class="fade-in">
-        <img src="imgs/y-combinator-logo.svg" alt="Y Combinator" height="50" class="fade-in">
-        <img src="imgs/forbes-logo.svg" alt="Forbes" height="40" class="fade-in">
-        <img src="imgs/tech-cruch-disrupt-logo.svg" alt="TechCrunch Disrupt" height="45" class="fade-in">
+    <!--     <div class="clients-container"> -->
+    <div class="scroller" data-direction="right" data-speed="slow">
+        <div class="scroller__inner">
+            <img src="imgs/accolades/fortune-500-logo.svg" alt="Fortune 500" height="60" class="">
+            <img src="imgs/accolades/y-combinator-logo.svg" alt="Y Combinator" height="50" class="">
+            <img src="imgs/accolades/forbes-logo.svg" alt="Forbes" height="40" class="">
+            <img src="imgs/accolades/tech-cruch-disrupt-logo.svg" alt="TechCrunch Disrupt" height="45" class="">
+            <img src="imgs/accolades/crunchbase.svg" alt="Crunchbase" height="40" class="">
+            <img src="imgs/accolades/HBR.svg" alt="harvard Business Review" height="40" class="">
+            <img src="imgs/accolades/Inc-5000.svg" alt="Inc 5000" height="40" class="">
+            <img src="imgs/accolades/webby-awards.svg" alt="Webby Awards" height="40" class="">
+            <img src="imgs/accolades/wired.svg" alt="Wired" height="40" class="">
+        </div>
     </div>
 </section>
