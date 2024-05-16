0f5b047a661e389e0559100c77ce6bb10f91b2f2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-05-16 13:28:24 +0200

    add: subscription confirmation page

---
+++ subscription-confirmation.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>🎉 Subscription Confirmed | Polyfen</title>
+
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+    <!-- PAGE-SPECIFIC META start -->
+    <meta name="robots" content="noindex, nofollow">
+    <meta property="og:url" content="https://polyfen.com/subscription-confirmed">
+    <!-- PAGE-SPECIFIC META end -->
+
+    <!-- PAGE-SPECIFIC CSS start -->
+    <!-- PAGE-SPECIFIC CSS end -->
+
+    <!-- PAGE-SPECIFIC SCRIPTS start -->
+    <!-- PAGE-SPECIFIC SCRIPTS end -->
+
+</head>
+
+<body class="landing-page dark-mode">
+    <!-- ya hay un estilo del founder-social  que llama a este ID -->
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P25JFWC" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <!-- INCLUDE HEADER.PHP start -->
+    <?php include 'includes/header.php';?>
+    <!-- INCLUDE HEADER.PHP end -->
+
+    <main>
+
+    </main>
+
+    <!-- INCLUDE FOOTER.PHP start -->
+    <?php include 'includes/footer.php';?>
+    <!-- INCLUDE FOOTER.PHP end -->
+
+</body>
+
+</html>
