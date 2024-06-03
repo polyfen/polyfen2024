5c7ff870b0c2de6fe504c020c2acca791e970407
xxiicode <leandrobilokapic@gmail.com>
2024-06-03 21:05:40 +0200

    improve: portfolio-thumbnails ->slider

--- includes/portfolio-thumbnails.php
+++ includes/portfolio-thumbnails.php
@@ -1,14 +1,18 @@
-<section class="container-1080 activity-box" aria-label="activity box">
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/podcasttoapp-app.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/metas-investments-business-cards.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/hemingway-distillery-whiskey-bottle.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/prepkanga-stationary.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/the-barn-packaging2.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/soma-iphone-app.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/health-and-travel-stationary.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/domingo.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/altirand-ios-app-icon.webp"></div>  
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/magusvalge-menu.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/mare-totebag.webp"></div>
-        <div class="activity-item fade-in"><img alt="" src="imgs/activity-cads/photographers-website.webp"></div>
+<section class="container- activity-box" aria-label="activity box">
+    <div class="scroller" data-direction="right" data-speed="slow">
+        <div class="scroller__inner">
+            <img alt="" src="imgs/activity-cads/podcasttoapp-app.webp">
+            <img alt="" src="imgs/activity-cads/metas-investments-business-cards.webp">
+            <img alt="" src="imgs/activity-cads/hemingway-distillery-whiskey-bottle.webp">
+            <img alt="" src="imgs/activity-cads/prepkanga-stationary.webp">
+            <img alt="" src="imgs/activity-cads/the-barn-packaging2.webp">
+            <img alt="" src="imgs/activity-cads/soma-iphone-app.webp">
+            <img alt="" src="imgs/activity-cads/health-and-travel-stationary.webp">
+            <img alt="" src="imgs/activity-cads/domingo.webp">
+            <img alt="" src="imgs/activity-cads/altirand-ios-app-icon.webp">
+            <img alt="" src="imgs/activity-cads/magusvalge-menu.webp">
+            <img alt="" src="imgs/activity-cads/mare-totebag.webp">
+            <img alt="" src="imgs/activity-cads/photographers-website.webp">
+        </div>
+    </div>
 </section>
