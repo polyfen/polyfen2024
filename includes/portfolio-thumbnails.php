5c1052aa5b527d426f0ce759aa670280de4b4f7f
xxiicode <leandrobilokapic@gmail.com>
2024-06-04 14:04:13 +0200

    improve: --gap 1rem

--- includes/portfolio-thumbnails.php
+++ includes/portfolio-thumbnails.php
@@ -1,4 +1,4 @@
-<section class="container- activity-box" aria-label="activity box">
+<section class="container- activity-box" aria-label="Portfolio thumbnails">
     <div class="scroller" data-direction="right" data-speed="slow">
         <div class="scroller__inner">
             <img alt="" src="imgs/activity-cads/podcasttoapp-app.webp">
