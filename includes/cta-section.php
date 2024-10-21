f26386a07e4531c31002bc137e2b1bbe40460902
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-10-21 10:47:58 -0300

    Refactor

--- includes/cta-section.php
+++ includes/cta-section.php
@@ -1,20 +1,20 @@
 <section class="section-cta">
     <!----- CTA SECTION GENERAL ----->
     <div class="cta-section-content-general flex-column-center">
-        <h2 class="align-center">Give your business the power-up it needs</h2>
+        <h2 class="title-2 align-center">Give your business the power-up it needs</h2>
         <a class="button large-button cta-button photo-button cta-button-book-call margin-bottom-05"  href="/discovery-call">Book Call</a>
         <a class="button large-button secondary-button cta-button-view-plans" data-button-arrow  href="/subscriptions">View Plans</a>
     </div>
     <!----- CTA SECTION GENERAL SUBSCRIPTIONS ----->
     <div class="cta-section-content-subscriptions  flex-column-center">
-        <h2 class="align-center margin-bottom-1">We knock out your tasks</h2>
-        <h3 class="align-center">You focus on growth</h3>
+        <h2 class="title-2 align-center margin-bottom-1">We knock out your tasks</h2>
+        <h3 class="heading-1 font-weight-400 align-center">You focus on growth</h3>
         <a data-button-arrow-up class="button large-button cta-button cta-button-subscriptions margin-bottom-05" href="#start-today">Start Today</a>
         <a class="button large-button secondary-button photo-button cta-button-book-call margin-bottom-05"  href="/discovery-call">Book Call</a>
     </div>
     <!----- CTA SECTION GENERAL BRANDING ----->
     <div class="cta-section-content-branding flex-column-center" id="book-call">
-        <h2 class="align-center">Book a Call to<br>Elevate your Brand</h2>
+        <h2 class="title-2 align-center">Book a Call to<br>Elevate your Brand</h2>
         <!---------- CALENDLY.php start ---------->
         <?php include 'includes/calendly.php';?>
         <!---------- CALENDLY.php end ---------->
