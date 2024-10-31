22b6c8b2089582ccb38f25f3aa4873055c0ee4f8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-10-31 12:13:00 -0300

    Add: 2nd row of portfolio thumbnails

--- includes/portfolio-scroller.php
+++ includes/portfolio-scroller.php
@@ -1,17 +1,43 @@
-<div class="activity-box fade-in" aria-label="Portfolio">
-    <div class="scroller" data-direction="right" data-speed="slow">
+<div class="activity-box" aria-label="Portfolio">
+    <div class="scroller fade-in" data-direction="right" data-speed="slow">
         <div class="scroller__inner">
-            <img alt="" src="imgs/activity-cads/podcasttoapp-app.webp">
-            <img alt="" src="imgs/activity-cads/metas-investments-business-cards.webp">
-            <img alt="" src="imgs/activity-cads/hemingway-distillery-whiskey-bottle.webp">
-            <img alt="" src="imgs/activity-cads/prepkanga-stationary.webp">
-            <img alt="" src="imgs/activity-cads/the-barn-packaging2.webp">
-            <img alt="" src="imgs/activity-cads/soma-iphone-app.webp">
-            <img alt="" src="imgs/activity-cads/health-and-travel-stationary.webp">
-            <img alt="" src="imgs/activity-cads/domingo.webp">
-            <img alt="" src="imgs/activity-cads/altirand-ios-app-icon.webp">
-            <img alt="" src="imgs/activity-cads/magusvalge-menu.webp">
-            <img alt="" src="imgs/activity-cads/photographers-website.webp">
+            <?php
+            $images = [
+                "imgs/activity-cads/lion-risk-management-logo.png",
+                "imgs/activity-cads/metas-investments-business-cards.webp",
+                "imgs/activity-cads/hemingway-distillery-whiskey-bottle.webp",
+                "imgs/activity-cads/prepkanga-stationary.webp",
+                "imgs/activity-cads/the-barn-packaging2.webp",
+                "imgs/activity-cads/alternation-logo.png",
+                "imgs/activity-cads/health-and-travel-stationary.webp",
+                "imgs/activity-cads/altirand-ios-app-icon.webp",
+                "imgs/activity-cads/photographers-website.webp"
+            ];
+            shuffle($images);
+            foreach ($images as $image) {
+                echo '<img alt="" src="' . $image . '">';
+            }
+            ?>
+        </div>
+    </div>
+    <div class="scroller fade-in" data-direction="right" data-speed="slow">
+        <div class="scroller__inner">
+            <?php
+            $images = [
+                "imgs/activity-cads/alchemy-container.png",
+                "imgs/activity-cads/podcasttoapp-app.png",
+                "imgs/activity-cads/mandala.webp",
+                "imgs/activity-cads/villa-terezija-totebag.png",
+                "imgs/activity-cads/magusvalge-menu.webp",
+                "imgs/activity-cads/lightning-bolt-logo.png",
+                "imgs/activity-cads/soma-iphone-app.webp",
+                "imgs/activity-cads/domingo-sign.png"
+            ];
+            shuffle($images);
+            foreach ($images as $image) {
+                echo '<img alt="" src="' . $image . '">';
+            }
+            ?>
         </div>
     </div>
 </div>
