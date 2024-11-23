42ec4ffb1c4c5f5c13086be6331ba1c5ddbcbdca
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-11-23 21:21:37 +0100

    update

--- includes/testimonials.php
+++ includes/testimonials.php
@@ -1,46 +1,79 @@
-    <div class="testimonials-container row gap-05">
-        <figure class="testimonial fade-in card card-with-padding col">
-            <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
-            <blockquote>
-                <p class="caption">
-                    Always a pleasure working with them. They are very professional
-                    and always deliver on time. Their work always exceeds expectations.
-                </p>
-            </blockquote>
-            <figcaption>
-                <img src="/imgs/testimonials/carlos-storie.webp" height="36" width="36" alt="">
-                <small>Carlos Flores,<br>CEO at Metas Investments</small>
-            </figcaption>
-
-        </figure>
-
-        <figure class="testimonial fade-in card card-with-padding col">
-            <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
-            <blockquote>
-                <p class="caption">
-                    The final result was really great and had tons of
-                    assets to use in different display formats.
-                    We are super happy with our brand!
-                </p>
-            </blockquote>
-            <figcaption>
-                <img src="/imgs/testimonials/mati-storie.webp" height="36" width="36" alt="">
-                <small> Matias Sanchez,<br>Founder at Bike in Mex</small>
-            </figcaption>
-
-        </figure>
-
-        <figure class="testimonial fade-in card card-with-padding col">
-            <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
-            <blockquote>
-                <p class="caption">
-                    Excellent skills and talent in project management, design, and branding.
-                </p>
-            </blockquote>
-            <figcaption>
-                <img src="/imgs/testimonials/cesar-storie.webp" height="36" width="36" alt="">
-                <small>Cesar Ramos,<br>CEO at Tradeana</small>
-            </figcaption>
-
-        </figure>
-    </div>
+<div class="testimonials-container testimonials-container-branding row gap-05">
+    <figure class="testimonial fade-in card card-with-padding col">
+        <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
+        <blockquote>
+            <p class="caption">
+                Always a pleasure working with them. They are very professional
+                and always deliver on time. Their work always exceeds expectations.
+            </p>
+        </blockquote>
+        <figcaption>
+            <img src="/imgs/testimonials/carlos-storie.webp" height="36" width="36" alt="">
+            <small>Carlos Flores,<br>CEO at Metas Investments</small>
+        </figcaption>
+    </figure>
+    <figure class="testimonial fade-in card card-with-padding col">
+        <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
+        <blockquote>
+            <p class="caption">
+                The final result was really great and had tons of
+                assets to use in different display formats.
+                We are super happy with our brand!
+            </p>
+        </blockquote>
+        <figcaption>
+            <img src="/imgs/testimonials/mati-storie.webp" height="36" width="36" alt="">
+            <small> Matias Sanchez,<br>Founder at Bike in Mex</small>
+        </figcaption>
+    </figure>
+    <figure class="testimonial fade-in card card-with-padding col">
+        <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
+        <blockquote>
+            <p class="caption">
+                Excellent skills and talent in project management, design, and branding.
+            </p>
+        </blockquote>
+        <figcaption>
+            <img src="/imgs/testimonials/cesar-storie.webp" height="36" width="36" alt="">
+            <small>Cesar Ramos,<br>CEO at Tradeana</small>
+        </figcaption>
+    </figure>
+</div>
+<div class="testimonials-container testimonials-container-staff-augmentation row gap-05">
+    <figure class="testimonial fade-in card card-with-padding col">
+        <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
+        <blockquote>
+            <p class="caption">
+                Thanks to their lightning-fast turnaround they helped us catch up with the product goals we were behind on.
+            </p>
+        </blockquote>
+        <figcaption>
+            <img src="/imgs/testimonials/carlos-storie.webp" height="36" width="36" alt="">
+            <small>#,<br>Founder at #</small>
+        </figcaption>
+    </figure>
+    <figure class="testimonial fade-in card card-with-padding col">
+        <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
+        <blockquote>
+            <p class="caption">
+                Every hire from Polyfen immediately clicked with the rest of team. Highly recommend.
+            </p>
+        </blockquote>
+        <figcaption>
+            <img src="/imgs/testimonials/mati-storie.webp" height="36" width="36" alt="">
+            <small>#,<br>CEO at #</small>
+        </figcaption>
+    </figure>
+    <figure class="testimonial fade-in card card-with-padding col">
+        <img src="/imgs/five-stars.svg" alt="5 stars" class="margin-bottom-1">
+        <blockquote>
+            <p class="caption">
+                Our whole integrations team was sourced by Polyfen. They are now a key part of our business.
+            </p>
+        </blockquote>
+        <figcaption>
+            <img src="/imgs/testimonials/cesar-storie.webp" height="36" width="36" alt="">
+            <small>#,<br>VP of Product at #</small>
+        </figcaption>
+    </figure>
+</div>
