dd5d771d3756691886579e919242276d2be20c0b
Boris Hrnčić <borishrncic@users.noreply.github.com>
2025-01-23 00:48:57 +0100

    #

--- includes/cta-section.php
+++ includes/cta-section.php
@@ -4,7 +4,7 @@
         <div class="content-wrapper">
             <h2 class="title-2 align-center">Give your business<br>the power-up it needs</h2>
             <a class="button large-button cta-button photo-button cta-button-book-call margin-bottom-05"  href="/discovery-call">Book Call</a>
-            <a class="button large-button secondary-button cta-button-view-plans" data-button-arrow  href="/subscriptions">View Plans</a>
+            <a class="button large-button secondary-button cta-button-view-plans" data-button-arrow  href="/ondemand">OnDemand Plans</a>
         </div>
         <div class="pattern-background">
         </div>
