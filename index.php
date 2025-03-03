b0b4e56e9ecfdf28910bffa56246fe10acfd002b
Boris Hrnčić <borishrncic@users.noreply.github.com>
2025-03-02 22:04:25 -0300

    #wip

--- index.php
+++ index.php
@@ -103,72 +103,81 @@ ___     _       __
             <h2 class="heading-1 align-center fade-in">Vertical Use Cases</h2>
             <div class="row margin-bottom-05">
                 <div class="col card fade-in">
-                    <div class="icon-container">
-                        <img src="/imgs/icons/clipboard.svg">
+                    <div class="mobile-row fade-in">
+                        <div class="icon-container">
+                            <img src="/imgs/icons/clipboard.svg">
+                        </div>
+                        <h3 class="heading-4 margin-bottom-1">Administrative</h3>
                     </div>
-                    <h3 class="heading-5 margin-bottom-1">Administrative</h3>
                     <ul class="hanging-list spaced-list">
-                        <li><strong>Automated Document Processing:</strong><br>Use AI to digitize, classify, and route documents, reducing manual input.</li>
-                        <li><strong>Scheduling & Calendar Management:</strong><br>AI-powered tools that suggest optimal meeting times and handle routine booking tasks.</li>
-                        <li><strong>Email & Communications Filtering:</strong><br>Intelligent prioritization and filtering of emails to streamline inbox management.</li>
-                        <li><strong>Workflow Automation:</strong><br>AI bots that handle repetitive admin tasks like data entry and invoice matching.</li>
-                        <li><strong>Smart Assistants:</strong><br>Voice-activated or chat-based AI systems that provide quick answers to internal administrative queries.</li>
-                        <li><strong>Legal & Regulatory Compliance:</strong> <br>Identify compliance risks, track policy changes, and monitor operational practices to ensure adherence to legal standards.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Automated Document Processing</h4><br>Use AI to digitize, classify, and route documents, reducing manual input.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Scheduling & Calendar Management</h4><br>AI-powered tools that suggest optimal meeting times and handle routine booking tasks.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Email & Communications Filtering</h4><br>Intelligent prioritization and filtering of emails to streamline inbox management.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Workflow Automation</h4><br>AI bots that handle repetitive admin tasks like data entry and invoice matching.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Smart Assistants</h4><br>Voice-activated or chat-based AI systems that provide quick answers to internal administrative queries.</li>
                     </ul>
                 </div>
                 <div class="col card fade-in">
-                    <div class="icon-container">
-                        <img src="/imgs/icons/users-three.svg">
+                    <div class="mobile-row fade-in">
+                        <div class="icon-container">
+                            <img src="/imgs/icons/users-three.svg">
+                        </div>
+                        <h3 class="heading-4 margin-bottom-1">Human Resources</h3>
                     </div>
-                    <h3 class="heading-5 margin-bottom-1">Human Resources</h3>
                     <ul class="hanging-list spaced-list">
-                        <li><strong>Talent Sourcing & Screening:</strong> <br>AI-driven tools that sort through resumes, matching candidates to open roles more efficiently.</li>
-                        <li><strong>Employee Engagement:</strong><br>Chatbots and sentiment analysis that gauge staff morale and highlight potential retention risks.</li>
-                        <li><strong>Onboarding & Training:</strong><br>Personalized learning paths and automated onboarding checklists for new hires.</li>
-                        <li><strong>Performance Evaluation:</strong> <br>AI-driven analytics that track KPIs and provide unbiased performance insights.</li>
-                        <li><strong>Predictive Workforce Planning:</strong> <br>Algorithms that forecast staffing needs based on historical data and upcoming projects.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Talent Sourcing & Screening</h4><br>AI-driven tools that sort through resumes, matching candidates to open roles more efficiently.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Employee Engagement</h4><br>Chatbots and sentiment analysis that gauge staff morale and highlight potential retention risks.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Onboarding & Training</h4><br>Personalized learning paths and automated onboarding checklists for new hires.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Performance Evaluation</h4><br>AI-driven analytics that track KPIs and provide unbiased performance insights.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Predictive Workforce Planning</h4><br>Algorithms that forecast staffing needs based on historical data and upcoming projects.</li>
                     </ul>
                 </div>
             </div>
             <div class="row margin-bottom-05">
-                <div class="col card fade-in">
-                    <div class="icon-container">
-                        <img src="/imgs/icons/wallet.svg">
+                <div class="col card fade-in">                
+                    <div class="mobile-row fade-in">
+                        <div class="icon-container">
+                            <img src="/imgs/icons/wallet.svg">
+                        </div>
+                        <h3 class="heading-4 margin-bottom-1">Finance</h3>
                     </div>
-                    <h3 class="heading-5 margin-bottom-1">Finance</h3>
                     <ul class="hanging-list spaced-list">
-                        <li><strong>Accounts Payable Automation:</strong><br>Automated invoice matching, payment approvals, and ledger updates.</li>
-                        <li><strong>Expense Management:</strong><br>Automated classification of expenses and anomaly detection to cut costs and reduce errors.</li>
-                        <li><strong>Tax Calculation:</strong><br>Automatic tax computation that accounts for varying regulations, deductions, and filing requirements to reduce manual effort and errors.</li>
-                        <li><strong>Financial Forecasting:</strong> <br>Predictive analytics that help with budgeting, revenue projections, and trend analysis.</li>
-                        <li><strong>Credit Scoring:</strong><br>AI-based risk assessment to make lending and credit decisions faster and more accurate.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Accounts Payable Automation</h4><br>Automated invoice matching, payment approvals, and ledger updates.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Expense Management</h4><br>Automated classification of expenses and anomaly detection to cut costs and reduce errors.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Tax Calculation</h4><br>Automatic tax computation that accounts for varying regulations, deductions, and filing requirements to reduce manual effort and errors.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Financial Forecasting</h4> <br>Predictive analytics that help with budgeting, revenue projections, and trend analysis.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Credit Scoring</h4><br>AI-based risk assessment to make lending and credit decisions faster and more accurate.</li>
                     </ul>
                 </div>
                 <div class="col card fade-in">
-                    <div class="icon-container">
-                        <img src="/imgs/icons/storefront.svg">
+                    <div class="mobile-row fade-in">
+                        <div class="icon-container">
+                            <img src="/imgs/icons/storefront.svg">
+                        </div>
+                        <h3 class="heading-4 margin-bottom-1">Marketing &amp; Sales</h3>
                     </div>
-                    <h3 class="heading-5 margin-bottom-1">Marketing &amp; Sales</h3>
                     <ul class="hanging-list spaced-list">
-                        <li><strong>Content Generation:</strong><br>AI-powered creation of blog posts, social media updates, and product descriptions tailored to specific audiences.</li>
-                        <li><strong>Targeted Campaigns:</strong><br>AI-powered segmentation and personalization to reach the right audiences at the right times.</li>
-                        <li><strong>Lead Scoring & Nurturing:</strong><br>Predictive models that identify high-potential leads and recommend tailored follow-ups.</li>
-                        <li><strong>Customer Insights:</strong><br>NLP-driven sentiment analysis to understand customer feedback and adapt marketing strategies.</li>
-                        <li><strong>Chatbots & Virtual Assistants:</strong><br>Automated customer interactions that handle FAQs, demo requests, and lead qualification.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Content Generation</h4><br>AI-powered creation of blog posts, social media updates, and product descriptions tailored to specific audiences.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Targeted Campaigns</h4><br>AI-powered segmentation and personalization to reach the right audiences at the right times.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Lead Scoring & Nurturing</h4><br>Predictive models that identify high-potential leads and recommend tailored follow-ups.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Customer Insights</h4><br>NLP-driven sentiment analysis to understand customer feedback and adapt marketing strategies.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Chatbots & Virtual Assistants</h4><br>Automated customer interactions that handle FAQs, demo requests, and lead qualification.</li>
                     </ul>
                 </div>
             </div>
             <div class="row">
                 <div class="col card fade-in">
-                    <div class="icon-container">
-                        <img src="/imgs/icons/shield.svg">
+                    <div class="mobile-row fade-in">
+                        <div class="icon-container">
+                            <img src="/imgs/icons/shield.svg">
+                        </div>
+                        <h3 class="heading-4 margin-bottom-1">Security</h3>
                     </div>
-                    <h3 class="heading-5 margin-bottom-1">Security</h3>
                     <ul class="hanging-list spaced-list">
-                        <li><strong>Fraud Detection:</strong><br>Machine learning models that spot suspicious transactions and unusual spending patterns in real time.</li>
-                        <li><strong>Real-time Threat Detection:</strong><br>AI systems that monitor network traffic, flag anomalies, and actively guard against potential attacks.</li>
-                        <li><strong>Automated Security Audits:</strong><br>Tools that regularly assess configurations, pinpoint vulnerabilities, and suggest proactive fixes.</li>
-                        <li><strong>Data Encryption & Access Control:</strong><br>Intelligent solutions that secure data at rest and in transit, ensuring only authorized users have access.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Fraud Detection</h4><br>Machine learning models that spot suspicious transactions and unusual spending patterns in real time.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Real-time Threat Detection</h4><br>AI systems that monitor network traffic, flag anomalies, and actively guard against potential attacks.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Automated Security Audits</h4><br>Tools that regularly assess configurations, pinpoint vulnerabilities, and suggest proactive fixes.</li>
+                        <li class="fade-in"><h4 class="heading-7 color-gradient">Data Encryption & Access Control</h4><br>Intelligent solutions that secure data at rest and in transit, ensuring only authorized users have access.</li>
                     </ul>
                 </div>
             </div>
