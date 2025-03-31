e7a44ad3a4ab0f45e5b8091caa2f72aa97a65581
Boris Hrnčić <borishrncic@users.noreply.github.com>
2025-03-31 10:04:58 -0300

    improve: culture section

--- index.php
+++ index.php
@@ -343,8 +343,8 @@ ___     _       __
             </div>
         </section>
         <hr>
-        <section class="section-cover-about fade-in visible">
-            <h2 class="title-4 margin-bottom-4 align-center">Our Culture,<br>Shaped by R&D</h2>
+        <section class="section-cover-about fade-in visible" id="culture">
+            <h2 class="title-4 margin-bottom-4 align-center">Innovative by Culture,<br>Not by Chance</h2>
             <div class="container-900 row gap-4">
                 <div class="col click-animation fade-in visible">
                     <img src="/imgs/cursor.svg" alt="" class="cursor-image">
@@ -354,9 +354,9 @@ ___     _       __
                 </div>
                 <div class="col">
                     <ul class="hanging-list spaced-list">
-                        <li class="fade-in"><h4 class="heading-6 font-weight-600 color-gradient">R&D at Our Core</h4><p class="margin-top-05">We invest heavily in R&D to stay cutting edge in AI. This keeps your solutions fresh, forward-looking, and highly competitive.</p></li>
-                        <li class="fade-in"><h4 class="heading-6 font-weight-600    color-gradient">Expert Team, Direct Impact</h4><p class="margin-top-05">We handpick specialists who innovate and solve real problems. By focusing on quality, every client gets dedicated attention.</p></li>
-                        <li class="fade-in"><h4 class="heading-6 font-weight-600 color-gradient">Scalable Results, Lasting Partnerships</h4><p class="margin-top-05">We don't just implement AI—we future-proof your operations. Our solutions grow with your business, automating workflows and refining decisions.</p></li>
+                        <li class="fade-in"><h4 class="heading-5 font-weight-600 color-gradient">R&D at Our Core</h4><p class="margin-top-05 fade-in">We invest heavily in R&D to stay at the forefront of AI. This empowers your business with cutting-edge tools to outperform competitors.</p></li>
+                        <li class="fade-in"><h4 class="heading-5 font-weight-600    color-gradient">Expert Team, Reliable Results</h4><p class="margin-top-05 fade-in">We hire only A-players with an extraordinary drive to excel. By focusing on quality, every client gets meaningful success.</p></li>
+                        <li class="fade-in"><h4 class="heading-5 font-weight-600 color-gradient">Long-Term Impact</h4><p class="margin-top-05 fade-in">We don't just implement AI—we future-proof your operations. Our solutions grow with your business, automating workflows and refining decisions.</p></li>
                     </ul>
                 </div>
             </div>
